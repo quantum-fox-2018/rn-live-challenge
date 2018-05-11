8c0da09d63c4774c514cc12c5ee3ab8a491df67b
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-05-11 10:47:03 +0700

    check point 2 done keyboard and main

--- rnChallenge/src/components/Keyboard.js
+++ rnChallenge/src/components/Keyboard.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
 class Keyboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  
-    };
+
+  constructor () {
+    super()
+    this.state = {
+      color: 'green',
+    }
+  }
+  guess = () => {
+    this.props.guessWords(this.props.alphabet)
+    this.setState({
+      color: 'red'
+    })
   }
   render() {
     return (
-      <View style={styles.alphaContainer}>
-        <Text>{this.props.alphabet}</Text>
+      <View style={{
+        marginTop: 12,
+        marginBottom: 12,
+        marginLeft: 12,
+        marginRight: 12,
+      }}>
+        <Button 
+        title={this.props.alphabet}
+        color={this.state.color}
+        onPress={this.guess}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  alphaContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    borderBottomColor: 'blue',
-  }
-})
+
 
 export default Keyboard;
--- rnChallenge/src/screens/MainScreen.js
+++ rnChallenge/src/screens/MainScreen.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react'
-import { Text, View} from 'react-native'
+import { Text, View, StyleSheet} from 'react-native'
 import Keyboard from '../components/Keyboard'
 class MainScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {  
-      alphabets: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y','z']
+      alphabets: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y','z'],
+      guessWords: [],
+      life: 5,
+      library: ['rama', 'diansyah', 'blablabla']
     };
   }
+
+  guessWords = (word) => {
+    let index = this.state.guessWords.findIndex( wordGuess => wordGuess === word)
+    if (index === -1) {
+      this.setState({
+        guessWords: [...this.state.guessWords, word]
+      })
+    }
+  }
+  
   render() {
-    let alphabet = this.state.alphabets.map( alphabet => 
-      <Keyboard alphabet={alphabet}/>
+    let alphabet = this.state.alphabets.map( (alphabet, index) => 
+      <Keyboard alphabet={alphabet} key={index} guessWords={this.guessWords}/>
     )
     return (
-      <View>
+      <View >
       <Text> Main Screen </Text>
-      {
-        alphabet
-      }
+      <View style={styles.alphaContainer}>
+        {
+          alphabet
+        }
+        </View>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  parentContainer: {
+
+  },
+  alphaContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 360,
+    borderBottomColor: 'blue',
+    borderBottomWidth: 1,
+    borderTopColor: 'blue',
+    borderTopWidth: 1,
+    borderRightColor: 'blue',
+    borderRightWidth: 1,
+    borderLeftColor: 'blue',
+    borderLeftWidth: 1,
+  }
+})
 export default MainScreen;
