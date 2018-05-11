838ac9a61eab0e4955e17be282939e8cae3bf0af
irsyadpahlapi <irsyadpahlapi@gmail.com>
2018-05-11 10:12:56 +0700

    tidak bisa ngebundle

---
+++ livecode/App.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Playing from './screens/Playing'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Guess CSS Props</Text>
+      </View>
+    );
+  }
+}
---
+++ livecode/screens/Playing.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+import { Text, View, Image, StyleSheet, Button, ScrollView } from 'react-native'
+
+class Playing extends Component {
+  render () {
+    return (
+      <View>
+        <Text>hallo</Text>
+      </View>
+    )
+  }
+}
+
+export default Playing
